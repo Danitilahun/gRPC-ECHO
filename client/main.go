28b69c5b01499cf3f306119ada79bcdc2e960a39
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-19 23:31:48 +0300

    Adding changes and make it work

--- client/main.go
+++ client/main.go
@@ -6,11 +6,10 @@ import (
 
 	hello "github.com/Danitilahun/gRPC-Hello_World.git/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
